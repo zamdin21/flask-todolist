162f81d43a875474e074fe48b49a564e75c9acfd
zamdin21 <msa.working@gmail.com>
2020-12-05 14:54:07 +0900

    GET Test

--- app.py
+++ app.py
@@ -1,40 +1,49 @@
 # https://www.atmarkit.co.jp/ait/articles/1807/31/news042.html
 from flask import Flask, render_template, redirect, request
-from todo import ToDoList
-
+from todo import ToDoList, init_db
 
 app = Flask(__name__)
 
+db = init_db(app)
+
 todolist = ToDoList()
+myname = 'shun'
 
 
-@app.route("/")
+@app.route('/')
 def show_todoliist():
-    return render_template("showtodo.html", todolist=todolist.get_all())
+    return render_template('showtodo.html', todolist=todolist.get_all())
 
 
-@app.route("/additem", methods=["POST"])
+@app.route('/additem', methods=['POST'])
 def add_item():
-    title = request.form["title"]
+    title = request.form['title']
     if not title:
-        return redirect("/")
+        return redirect('/')
     todolist.add(title)
-    return redirect("/")
+    return redirect('/')
 
 
-@app.route("/deleteitem/<int:item_id>")
+@app.route('/deleteitem/<int:item_id>')
 def delete_todoitem(item_id):
     todolist.delete(item_id)
-    return redirect("/")
+    return redirect('/')
 
 
-@app.route("/updatedone/<int:item_id>")
-def update_todoitemdone(item_id):
-    todolist.update(item_id)
-    return redirect("/")
+@app.route('/updatedone', methods=['POST'])
+def update_done(item_id):
+    keys = request.form.keys()
+    items = [int(x) for x in keys]
+    todolist.update_done(items)
+    return redirect('/')
 
 
-@app.route("/deletealldoneitems")
+@app.route('/deletealldoneitems')
 def delete_alldoneitems():
     todolist.delete_doneitem()
-    return redirect("/")
+    return redirect('/')
+
+
+@app.route('/hello', methods=['GET'])
+def hello_test():
+    return render_template('hello.html', myname=myname, say=request.args.get('say'), to=request.args.get('to'))
--- todo.py
+++ todo.py
@@ -1,52 +1,57 @@
 from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy
+db = SQLAlchemy()
 
 
 def init_db(app):
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db/sample.db"
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/todoitems.db'
     db.init_app(app)
 
 
 class ToDoItem(db.Model):
-    __tablename__ = "todoitems"
+    __tablename__ = 'todoitems'
     item_id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     done = db.Column(db.Boolean, nullable=False, default=False)
 
 
 class ToDoList:
-    def __init__(self):
-        self.todolist = []
-
     def add(self, title):
         """
-        self.todoListにToDo項目を追加する
+        todoitemsデータベースにToDoItemを追加する
         """
-        item = ToDoItem(title)
-        self.todolist.append(item)
+        item = ToDoItem(title=title)
+        db.session.add(item)
+        db.session.commit()
 
     def delete(self, item_id):
         """
-        指定されたIDのToDo項目を削除する
+        todoitemsデータベースから指定されたIDのToDoItemを削除する
         """
-        self.todolist = [x for x in self.todolist if x.item_id != item_id]
+        item = ToDoItem.query.filter_by(item_id=item_id).first()
+        db.session.delete(item)
+        db.session.commit()
 
-    def update(self, item_id):
-        """
-        指定されたIDのdoneメンバの値を反転する
-        """
-        item = [x for x in self.todolist if x.item_id == item_id]
-        item[0].done = not item[0].done
-
-    def get_all(self,):
+    def get_all(self):
         """
         ToDo項目を含むリストを返送する
         """
-        return self.todolist
+        items = ToDoItem.query.all()
+        return items
 
     def delete_doneitem(self):
         """
         doneメンバの値がTrueとなっている項目（完了した項目）をリストから削除する
         """
-        self.todolist = [x for x in self.todolist if not x.done]
+        ToDoItem.query.filter_by(done=True).delete()
+        db.session.commit()
+
+    def update_done(self, items):
+        """
+        指定されたIDのdoneメンバの値を反転する
+        """
+        for item in self.get_all():
+            if item.item_id in items:
+                item.done = True
+            else:
+                item.done = False
+        db.session.commit()
