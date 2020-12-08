0eb7bb1c35066b3041dc4e668e086f28a7a89257
zamdin21 <msa.working@gmail.com>
2020-12-08 19:30:29 +0900

    Check test

--- app.py
+++ app.py
@@ -1,49 +1,47 @@
-# https://www.atmarkit.co.jp/ait/articles/1807/31/news042.html
-from flask import Flask, render_template, redirect, request
-from todo import ToDoList, init_db
+# https://www.atmarkit.co.jp/ait/articles/1808/21/news036.html
+from flask import Flask, jsonify, request
+from todo import ToDoItem, init_db, init_schema
+from todo import item_schema, items_schema
 
 app = Flask(__name__)
 
 db = init_db(app)
-
-todolist = ToDoList()
-myname = 'shun'
+init_schema(app)
 
 
-@app.route('/')
-def show_todoliist():
-    return render_template('showtodo.html', todolist=todolist.get_all())
+@app.route('/api/todoitems', methods=['GET'])
+def get_todoliist():
+    items = ToDoItem.query.all()
+    return items_schema.jsonify(items)
 
 
-@app.route('/additem', methods=['POST'])
-def add_item():
-    title = request.form['title']
-    if not title:
-        return redirect('/')
-    todolist.add(title)
-    return redirect('/')
+@app.route('/api/todoitems/<int:item_id>', methods=['GET'])
+def get_todoitem(item_id):
+    item = ToDoItem.query.filter_by(item_id=item_id).first_or_404()
+    return item_schema.jsonify(item)
 
 
-@app.route('/deleteitem/<int:item_id>')
+@app.route('/api/todoitems', methods=['POST'])
+def add_todoitem():
+    if not 'title' in request.json:
+        return 'error'
+    item = ToDoItem(title=request.json['title'], done=False)
+    db.session.add(item)
+    db.session.commit()
+    return item_schema.jsonify(item)
+
+
+@app.route('/api/todoitems/<int:item_id>', methods=['DELETE'])
 def delete_todoitem(item_id):
-    todolist.delete(item_id)
-    return redirect('/')
+    item = ToDoItem.query.filter_by(item_id=item_id).first_or_404()
+    db.session.delete(item)
+    db.session.commit()
+    return jsonify({'result': True})
 
 
-@app.route('/updatedone', methods=['POST'])
-def update_done():
-    keys = request.form.keys()
-    items = [int(x) for x in keys]
-    todolist.update_done(items)
-    return redirect('/')
-
-
-@app.route('/deletealldoneitems')
-def delete_alldoneitems():
-    todolist.delete_doneitem()
-    return redirect('/')
-
-
-@app.route('/hello', methods=['GET'])
-def hello_test():
-    return render_template('hello.html', myname=myname, say=request.args.get('say'), to=request.args.get('to'))
+@app.route('/api/todoitems/<int:item_d>', methods=['PUT'])
+def update_todoitem(item_id):
+    item = ToDoItem.query.filter_by(item_id=item_id).first_or_404()
+    item.done = not item.done
+    db.session.commit()
+    return item_schema.jsonify(item)
--- todo.py
+++ todo.py
@@ -1,10 +1,18 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+
 db = SQLAlchemy()
+ma = Marshmallow()
 
 
 def init_db(app):
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/todoitems.db'
     db.init_app(app)
+    return db
+
+
+def init_schema(app):
+    ma.init_app(app)
 
 
 class ToDoItem(db.Model):
@@ -14,44 +22,10 @@ class ToDoItem(db.Model):
     done = db.Column(db.Boolean, nullable=False, default=False)
 
 
-class ToDoList:
-    def add(self, title):
-        """
-        todoitemsデータベースにToDoItemを追加する
-        """
-        item = ToDoItem(title=title)
-        db.session.add(item)
-        db.session.commit()
+class ItemSchema(ma.Schema):
+    class Meta:
+        fields = ("item_id", "title", "done")
 
-    def delete(self, item_id):
-        """
-        todoitemsデータベースから指定されたIDのToDoItemを削除する
-        """
-        item = ToDoItem.query.filter_by(item_id=item_id).first()
-        db.session.delete(item)
-        db.session.commit()
 
-    def get_all(self):
-        """
-        ToDo項目を含むリストを返送する
-        """
-        items = ToDoItem.query.all()
-        return items
-
-    def delete_doneitem(self):
-        """
-        doneメンバの値がTrueとなっている項目（完了した項目）をリストから削除する
-        """
-        ToDoItem.query.filter_by(done=True).delete()
-        db.session.commit()
-
-    def update_done(self, items):
-        """
-        指定されたIDのdoneメンバの値を反転する
-        """
-        for item in self.get_all():
-            if item.item_id in items:
-                item.done = True
-            else:
-                item.done = False
-        db.session.commit()
+item_schema = ItemSchema()
+items_schema = ItemSchema(many=True)
