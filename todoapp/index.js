423b6dd373cb36917db9ab93fb73708211c79e11
zamdin21 <msa.working@gmail.com>
2020-12-18 10:39:43 +0900

    Fix index.js

--- todoapp/index.js
+++ todoapp/index.js
@@ -1,49 +1,50 @@
-const reques_url="http://localhost:5000/api/todoitems";
+const request_url = "http://localhost:5000/api/todoitems";
 const bus = new Vue();
 
-Vue.component("show-todo-items",{
-    data:function(){return{items:null}; },
-    methods:{
-        showtodo:function(){
-            fetch(reques_url)
-            .then(response => response.json())
-            .then(jsondata => this.items=jsondata.objects);
-        }
-    },
-    mounted: function(){
-        this.showtodo();
-        bus.$on("on-item-registerd",this.showtodo);
-    },
-    template: '<ul><li v-for="item in items">{{item.title}}</li></ul>'
+Vue.component("show-todo-items", {
+  data: function () { return { items: null }; },
+  methods: {
+    showtodo: function () {
+      fetch(request_url)
+        .then(response => response.json())
+        .then(jsondata => this.items = jsondata.objects);
+    }
+  },
+  mounted: function () {
+    this.showtodo();
+    bus.$on("on-item-registered", this.showtodo);
+  },
+  template: '<ul><li v-for="item in items">{{ item.title }}</li></ul>'
 });
 
-Vue.component("register-todo-item",{
-    data:function(){return{todotitle:""};},
-    template:
+Vue.component("register-todo-item", {
+  data: function () { return { todotitle: "" }; },
+  template:
     `<div>
-    <input v-model="todotitle">
-    <button v-on:click="register_todoitem(todotitle)">Register todo item</button>
+      <input v-model="todotitle">
+      <button v-on:click="register_todoitem(todotitle)">Register todo item</button>
     </div>`,
-    methods:{
-        register_todoitem:function(todotitle){
-            concole.log(todotitle);
-            fetch(request_url,{
-                method:"POST",
-                mode:"cors",
-                body:JSON.stringify({title:todotitle}),
-                headers:{
-                    "content-type":"application/json"
-                }
-            })
-            .then(response=>response.json())
-            .then(jsondata=>{
-                console.log(jsondata);
-                bus.$emit("on-item-registerd",jsondata);
-            })
-            .catch(err=>console.log(`err:${err}`));
+  methods: {
+    register_todoitem: function(todotitle) {
+      console.log(todotitle);
+      fetch(request_url, {
+        method: "POST",
+        mode: "cors",
+        body: JSON.stringify({ title: todotitle }),
+        headers: {
+          "content-type": "application/json"
         }
+      })
+        .then(response => response.json())
+        .then(jsondata => {
+          console.log(jsondata);
+          bus.$emit("on-item-registered", jsondata);
+        })
+        .catch(err => console.log(`err: ${err}`));
     }
-    });
-    var app=new Vue({
-        el:'#app'
-    });
+  }
+});
+
+var app = new Vue({
+  el: '#app'
+});
