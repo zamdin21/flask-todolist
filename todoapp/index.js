78c5022b44c6dd65b30d650f08c18506c71047d0
zamdin21 <msa.working@gmail.com>
2020-12-18 10:13:39 +0900

    web api created

---
+++ todoapp/index.js
@@ -0,0 +1,49 @@
+const reques_url="http://localhost:5000/api/todoitems";
+const bus = new Vue();
+
+Vue.component("show-todo-items",{
+    data:function(){return{items:null}; },
+    methods:{
+        showtodo:function(){
+            fetch(reques_url)
+            .then(response => response.json())
+            .then(jsondata => this.items=jsondata.objects);
+        }
+    },
+    mounted: function(){
+        this.showtodo();
+        bus.$on("on-item-registerd",this.showtodo);
+    },
+    template: '<ul><li v-for="item in items">{{item.title}}</li></ul>'
+});
+
+Vue.component("register-todo-item",{
+    data:function(){return{todotitle:""};},
+    template:
+    `<div>
+    <input v-model="todotitle">
+    <button v-on:click="register_todoitem(todotitle)">Register todo item</button>
+    </div>`,
+    methods:{
+        register_todoitem:function(todotitle){
+            concole.log(todotitle);
+            fetch(request_url,{
+                method:"POST",
+                mode:"cors",
+                body:JSON.stringify({title:todotitle}),
+                headers:{
+                    "content-type":"application/json"
+                }
+            })
+            .then(response=>response.json())
+            .then(jsondata=>{
+                console.log(jsondata);
+                bus.$emit("on-item-registerd",jsondata);
+            })
+            .catch(err=>console.log(`err:${err}`));
+        }
+    }
+    });
+    var app=new Vue({
+        el:'#app'
+    });
